0fdd4f12c28b65cde495c6161c2f7617b7105b82
Rat <ratulbhowmick66@gmail.com>
2021-01-02 20:34:21 +0600

    check

--- famheir/src/App.js
+++ famheir/src/App.js
@@ -1,9 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Login from "./component/Auth/Login";
+import TopNav from "./component/Layout/TopNav";
 
 function App() {
-  return <Login />;
+  return <TopNav />;
 }
 
 export default App;
